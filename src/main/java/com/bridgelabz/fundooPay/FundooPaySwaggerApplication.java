6ad184c3ccae03a3b7ce89c549f258af1157e454
Uma Shankar <shankar.uma35@gmail.com>
2017-11-17 10:23:15 +0530

    Modified Property file

--- src/main/java/com/bridgelabz/fundooPay/FundooPaySwaggerApplication.java
+++ src/main/java/com/bridgelabz/fundooPay/FundooPaySwaggerApplication.java
@@ -2,8 +2,10 @@ package com.bridgelabz.fundooPay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.bridgelabz.fundooPay")
 public class FundooPaySwaggerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bridgelabz/fundooPay/controller/HomeController.java
+++ src/main/java/com/bridgelabz/fundooPay/controller/HomeController.java
@@ -3,15 +3,15 @@ package com.bridgelabz.fundooPay.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import io.swagger.annotations.ApiModelProperty;
-@Controller
+import io.swagger.annotations.Api;
 
+@Controller
+@Api(hidden = true)
 public class HomeController {
 
 	@RequestMapping("/")
-	@ApiModelProperty(hidden = true)
-    public String home() {
-        return "redirect:swagger-ui.html";
-    }
-	
+	public String home() {
+		return "redirect:swagger-ui.html";
+	}
+
 }
